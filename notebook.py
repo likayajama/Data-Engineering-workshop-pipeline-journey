985207760a1981274df4b1b98db671743c2815f2
likayajama <likhayajama@gmail.com>
2026-06-11 18:25:03 +0200

    notebook

---
+++ notebook.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[87]:
+
+
+import pandas as pd
+
+
+# In[88]:
+
+
+prefix = ' https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+url = f'{prefix}/yellow_tripdata_2021-01.csv.gz'
+url
+
+
+# In[89]:
+
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+df = pd.read_csv(
+    url,
+    dtype=dtype,
+    parse_dates=parse_dates
+) 
+
+
+# In[90]:
+
+
+get_ipython().system('uv add psycopg2-binary')
+
+
+# In[91]:
+
+
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://root:root@localhost:5433/ny_taxi')
+
+
+# In[92]:
+
+
+df.head(0)
+
+
+# In[93]:
+
+
+df. head(0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
+
+
+# In[94]:
+
+
+df_iter = pd.read_csv(
+    url,
+    dtype=dtype,
+    parse_dates=parse_dates,
+    iterator=True,
+    chunksize=100000,
+)
+
+
+# In[95]:
+
+
+from tqdm.auto import tqdm
+
+
+# In[96]:
+
+
+for df_chunk in tqdm(df_iter):
+    df_chunk.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+
+
+# In[ ]:
+
+
+
+
