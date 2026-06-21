2ce3f921f8d6db409fe566e7fc1c60507c5991ac
likayajama <likhayajama@gmail.com>
2026-06-21 19:00:22 +0200

    ingestion script

--- ingest_data.py
+++ ingest_data.py
@@ -3,6 +3,7 @@
 
 import pandas as pd
 from calendar import month
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
@@ -30,17 +31,16 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = '5433'
-    pg_database = 'ny_taxi'
-
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
+@click.command()
+@click.option("--pg-user", default="root", show_default=True, help="Postgres user")
+@click.option("--pg-password", default="root", show_default=True, help="Postgres password")
+@click.option("--pg-host", default="localhost", show_default=True, help="Postgres host")
+@click.option("--pg-port", default=5433, type=int, show_default=True, help="Postgres port")
+@click.option("--pg-database", default="ny_taxi", show_default=True, help="Postgres database")
+@click.option("--year", default=2021, type=int, show_default=True, help="Year of data to download")
+@click.option("--month", default=1, type=int, show_default=True, help="Month of data to download")
+@click.option("--target-table", default="yellow_taxi_data", show_default=True, help="Target Postgres table")
+def run(pg_user, pg_password, pg_host, pg_port, pg_database, year, month, target_table):
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
